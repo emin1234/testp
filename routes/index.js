c29e01e56380127d00af7137a29d5552fb24b8ae
Emin Dukic <emindukic123@gmail.com>
2019-09-21 19:32:57 +0200

    Icommit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var custome = require('../DB/db');
+var customer = require('../DB/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
